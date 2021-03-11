1cda77d3c3d5d034b155e38c8b948c8c2a430113
hsiaofongw <hsiaofong.w@gmail.com>
2021-03-12 07:15:53 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,47 @@
+async function headers() {
+    return [
+        {
+            source: '/friends',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'public,max-age=259200',
+                },
+                {
+                    key: 'X-My-Comment',
+                    value: 'you3 lian4 wei4 zhao1 zu1, xiang2 qing2 qing3 fa1 song4 you2 jian4 zi1 xun2: i@beyondstars.xyz'
+                },
+                {
+                    key: 'X-My-Comment-Encoded',
+                    value: 'U+53CB U+94FE U+4F4D U+62DB U+79DF U+8BE6 U+60C5 U+8BF7 U+53D1 U+9001 U+90AE U+4EF6 U+54A8 U+8BE2 : i @ b e y o n d s t a r s . x y z'
+                },
+                {
+                    key: 'X-My-Feeling',
+                    value: 'ha ha ha ha ha ha ha ha'
+                }
+            ]
+        },
+        {
+            source: '/',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'public,max-age=259200'
+                }
+            ]
+        },
+        {
+            source: '/abouts',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'public,max-age=259200'
+                }
+            ]
+        }
+    ]
+}
+
 async function rewrites() {
 
     let routes = [];
@@ -16,5 +60,5 @@ async function rewrites() {
 }
 
 module.exports = {
-    rewrites
+    rewrites, headers
 }
