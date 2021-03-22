93128926732ec8e8a7f4668313d67e5069e4cafd
hsiaofongw <hsiaofong.w@gmail.com>
2021-03-22 10:09:56 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,27 +1,24 @@
 async function headers() {
-    return [
-        {
-            source: '/:slug',
-            headers: [
-                {
-                    key: 'Cache-Control',
-                    value: 'public,max-age=60',
-                },
-                {
-                    key: 'X-My-Comment',
-                    value: 'you3 lian4 wei4 zhao1 zu1, xiang2 qing2 qing3 fa1 song4 you2 jian4 zi1 xun2: i@beyondstars.xyz'
-                },
-                {
-                    key: 'X-My-Comment-Encoded',
-                    value: 'U+53CB U+94FE U+4F4D U+62DB U+79DF U+8BE6 U+60C5 U+8BF7 U+53D1 U+9001 U+90AE U+4EF6 U+54A8 U+8BE2 : i @ b e y o n d s t a r s . x y z'
-                },
-                {
-                    key: 'X-My-Feeling',
-                    value: 'ha ha ha ha ha ha ha ha'
-                }
-            ]
-        }
-    ]
+
+    const sources = {
+        'indexPage': '/',
+        'directoryPage': '/:slug'
+    };
+
+    const oneMinuteCache = {
+        key: 'Cache-Control',
+        value: 'public,max-age=60',
+    }
+
+    let rules = [];
+    for (const key in sources) {
+        const source = sources[key];
+        rules.push({
+            source, headers: [ oneMinuteCache ]
+        });
+    }
+
+    return rules;
 }
 
 async function rewrites() {
