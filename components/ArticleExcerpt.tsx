867b21027e7b6a546730b6ca4ace1c8c1e6d2bbf
hsiaofongw <hsiaofong.w@gmail.com>
2021-03-23 15:25:08 +0800

    add semantics

--- components/ArticleExcerpt.tsx
+++ components/ArticleExcerpt.tsx
@@ -7,17 +7,19 @@ export class ArticleExcerpt extends React.Component<IPostExcerptData, {}> {
     render() {
         const title = this.props.name;
         const description = this.props.description;
-        const date = new Date(this.props.date).toLocaleDateString(
-            'en-us', { month: 'short', day: 'numeric' }
-        );
         const file = this.props.prettyPath || this.props.file;
         // const file = this.props.file;
+
+        let dateObject = new Date(this.props.date);
+        const dateString = dateObject.toLocaleDateString(
+            'en-us', { month: 'short', day: 'numeric' }
+        );
 
         return (
             <HoverTransitionListItem>
                 <a href={file}>
                     <article>
-                        <time dateTime={date} className="text-sm mb-1 text-greenandgray-base01">{date}</time>
+                        <time dateTime={dateObject.toISOString()} className="text-sm mb-1 text-greenandgray-base01">{dateString}</time>
                         <Heading2>
                             {title}
                         </Heading2>
